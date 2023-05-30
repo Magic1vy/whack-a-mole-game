33667ca366f090bec71fec4125c571ba4911ec1d
Valeriia Isaienko <93624578+Magic1vy@users.noreply.github.com>
2023-05-29 17:30:30 -0700

    second commit

--- script.js
+++ script.js
@@ -1,150 +1,68 @@
-// const holes = document.querySelectorAll('.hole');
-// const scoreBoard = document.querySelector('.score');
-// const moles = document.querySelectorAll('.mole');
-// const startBtn = document.querySelector("#start");
-// const intermediateBtn = document.querySelector("#intermediate");
-// const advanceBtn = document.querySelector("#advance");
-// const scores = JSON.parse(localStorage.getItem('score')) || [];
-// const lastScoreElement = document.querySelector('.last-score');
-
-
-// let lastHole;
-// let timeUp = false;
-// let score = 0;
-// let isClicked = false;
-
-// window.addEventListener('DOMContentLoaded', () => {
-//     const savedScores = JSON.parse(localStorage.getItem('score'));
-//     if (savedScores && savedScores.length > 0) {
-//         const lastScore = savedScores[savedScores.length - 1];
-//         lastScoreElement.textContent = lastScore;
-//     }
-// });
-
-// function randomTime(min, max) {
-//     return Math.round(Math.random() * (max - min) + min);
-// }
-
-// function randomHole(holes){
-//     const idx = Math.floor(Math.random() * holes.length);
-//     const hole = holes[idx];
-//     if (hole === lastHole){
-//         randomHole(holes);
-//     }
-
-//     lastHole = hole; 
-//     return hole;
-// }
-
-// function peep(timeRange) {
-//     const time = randomTime(timeRange.min, timeRange.max);
-//     const hole = randomHole(holes);
-//     hole.classList.add("up");
-//     localStorage.setItem('score', JSON.stringify(scores));
-//     setTimeout(() => {
-//         hole.classList.remove("up");
-//         if (!timeUp) peep(timeRange);
-//     }, time);
-// }
-
-// function startGame(timeRange) {
-//     scoreBoard.textContent = 0;
-//     timeUp = false;
-//     score = 0;
-//     peep(timeRange);
-//     setTimeout(() => (timeUp = true), 10000);
-//     localStorage.setItem('score', JSON.stringify(scores));
-// }
-
-// function bonk(e) {
-//     if (!e.isTrusted) return;
-//     isClicked = true;
-//     score++;
-//     this.classList.remove('up');
-//     scoreBoard.textContent = score;
-
-//     scores.push(score);
-//     localStorage.setItem('score', JSON.stringify(scores));
-
-//     lastScoreElement.textContent = score;
-// }
-
-// startBtn.addEventListener("click", () => startGame({ min: 250, max: 1300 }));
-// intermediateBtn.addEventListener("click", () => startGame({ min: 200, max: 1000 }));
-// advanceBtn.addEventListener("click", () => startGame({ min: 50, max: 400 }));
-// moles.forEach(mole => mole.addEventListener("click", bonk));
-
-
-
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
 const startBtn = document.querySelector("#start");
 const intermediateBtn = document.querySelector("#intermediate");
 const advanceBtn = document.querySelector("#advance");
-const scores = JSON.parse(localStorage.getItem('score')) || [];
-const lastScoreElement = document.querySelector('.last-score');
+const bestScoreElement = document.querySelector('.best-score');
 
 let lastHole;
 let timeUp = false;
 let score = 0;
 let isClicked = false;
+let bestScore = parseInt(localStorage.getItem('bestScore')) || 0;
 
 window.addEventListener('DOMContentLoaded', () => {
-    const savedScores = JSON.parse(localStorage.getItem('score'));
-    if (savedScores && savedScores.length > 0) {
-        const lastScore = savedScores[savedScores.length - 1];
-        lastScoreElement.textContent = lastScore;
-    }
+  bestScoreElement.textContent = bestScore;
 });
 
 function randomTime(min, max) {
-    return Math.round(Math.random() * (max - min) + min);
+  return Math.round(Math.random() * (max - min) + min);
 }
 
-function randomHole(holes){
-    const idx = Math.floor(Math.random() * holes.length);
-    const hole = holes[idx];
-    if (hole === lastHole){
-        randomHole(holes);
-    }
-    console.log(lastHole);
-    lastHole = hole; 
-    return hole;
+function randomHole(holes) {
+  const idx = Math.floor(Math.random() * holes.length);
+  const hole = holes[idx];
+  if (hole === lastHole) {
+    randomHole(holes);
+  }
+
+  lastHole = hole;
+  return hole;
 }
 
 function peep(timeRange) {
-    const time = randomTime(timeRange.min, timeRange.max);
-    const hole = randomHole(holes);
-    hole.classList.add("up");
-    localStorage.setItem('score', JSON.stringify(scores));
-    isClicked = false;
-    setTimeout(() => {
-        hole.classList.remove("up");
-        if (!timeUp) peep(timeRange);
-    }, time);
+  const time = randomTime(timeRange.min, timeRange.max);
+  const hole = randomHole(holes);
+  hole.classList.add("up");
+  localStorage.setItem('bestScore', bestScore);
+  isClicked = false;
+  setTimeout(() => {
+    hole.classList.remove("up");
+    if (!timeUp) peep(timeRange);
+  }, time);
 }
 
 function startGame(timeRange) {
-    scoreBoard.textContent = 0;
-    timeUp = false;
-    score = 0;
-    peep(timeRange);
-    setTimeout(() => (timeUp = true), 10000);
-    localStorage.setItem('score', JSON.stringify(scores));
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  score = 0;
+  peep(timeRange);
+  setTimeout(() => (timeUp = true), 10000);
 }
 
 function bonk(e) {
-    if (!e.isTrusted || isClicked) return;
-    isClicked = true;
-    score++;
-    this.classList.remove('up');
-    scoreBoard.textContent = score;
+  if (!e.isTrusted || isClicked) return;
+  isClicked = true;
+  score++;
+  this.classList.remove('up');
+  scoreBoard.textContent = score;
 
-    scores.push(score);
-    localStorage.setItem('score', JSON.stringify(scores));
-
-    lastScoreElement.textContent = score;
+  if (score > bestScore) {
+    bestScore = score;
+    localStorage.setItem('bestScore', bestScore);
+    bestScoreElement.textContent = bestScore;
+  }
 }
 
 startBtn.addEventListener("click", () => startGame({ min: 250, max: 1300 }));
